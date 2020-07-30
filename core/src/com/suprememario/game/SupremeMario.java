73b1ef9d8afbd560390506e1dae39a406ce2549e
vinuruban <vinu_06@hotmail.com>
2020-07-30 16:07:37 +0100

    Mario drops from center to the ground at game start. Click to make Mario fly. He cannot fly beyond the screen.

--- core/src/com/suprememario/game/SupremeMario.java
+++ core/src/com/suprememario/game/SupremeMario.java
@@ -12,6 +12,9 @@ public class SupremeMario extends ApplicationAdapter {
 	Texture[] mario; //array since multiple frames are added
 	int marioState = 0; //to loop through each frames of Mario
 	int pause = 0; //to slow down animation speed since render() speed is fast
+	float gravity = 0.8f;
+	float velocity = 0;
+	int marioY = 0; //y-position of mario
 	
 	@Override
 	public void create () { //create() method gets called onCreate
@@ -26,6 +29,9 @@ public class SupremeMario extends ApplicationAdapter {
 		mario[1] = new Texture("frame-2.png");
 		mario[2] = new Texture("frame-3.png");
 		mario[3] = new Texture("frame-4.png");
+
+		//center mario
+		marioY = Gdx.graphics.getHeight() / 2;
 	}
 
 	@Override
@@ -44,7 +50,27 @@ public class SupremeMario extends ApplicationAdapter {
 				marioState = 0;
 			}
 		}
-		batch.draw(mario[marioState], (Gdx.graphics.getWidth() / 2) - (mario[0].getWidth() / 2), (Gdx.graphics.getHeight() / 2) - (mario[0].getHeight() / 2)); //centers mario and loops through all 3 frames!
+
+		//to make Mario jump by clicking mouse
+		if (Gdx.input.justTouched()) {
+			velocity = -25; //this negative will add to marioY value below!
+		}
+
+		velocity += gravity; //add 0.8f to velocity at every render() loop
+		marioY -= velocity; //decrease y-position of Mario
+
+		//to stop mario from sinking into the ground
+		if (marioY <= 0) {
+			marioY = 0;
+		}
+
+		//to stop mario from flying above the screen
+		int maxHeight = Gdx.graphics.getHeight() - mario[0].getWidth();
+		if (marioY >= maxHeight) {
+			marioY =  maxHeight;
+		}
+
+		batch.draw(mario[marioState], (Gdx.graphics.getWidth() / 2) - (mario[0].getWidth() / 2), marioY); //centers mario and loops through all 3 frames!
 
 		batch.end();
 	}
